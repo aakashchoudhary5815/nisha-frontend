edafa128504d5608bedd2e7b214f330b1e360d8b
Aakash Choudhary <52340133+aakashchoudhary5815@users.noreply.github.com>
2024-02-10 18:25:02 +0530

    Update CartItems.jsx

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -21,7 +21,7 @@ const CartItems = () => {
         {
             return <div>
                         <div className="cartitems-format cartitems-format-main">
-                            <img src={e.image} alt="" className='carticon-product-icon' />
+                            <img src={`https://backend-1-moxo.onrender.com/images/${e.image.split("/").pop()}`} alt="" className='carticon-product-icon' />
                             <p>{e.name}</p>
                             <p>${e.new_price}</p>
                             <button className='cartitems-quantity'>{cartItems[e.id]}</button>
